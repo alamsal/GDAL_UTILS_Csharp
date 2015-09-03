d2b7b0457832f175aae73103bbe024f67738f9af
unknown <aashislamsal@AFSPCHPA23810BJ.usda.net>
2015-09-03 11:13:22 -0600

    Framework ready for zonal statistics

--- RasterizeCsharp/RasterizeCsharp/VectorToRaster.cs
+++ RasterizeCsharp/RasterizeCsharp/VectorToRaster.cs
@@ -1,4 +1,5 @@
-﻿using RasterizeCsharp.RasterizeLayer;
+﻿using System;
+using RasterizeCsharp.RasterizeLayer;
 
 namespace RasterizeCsharp
 {
@@ -12,8 +13,10 @@ namespace RasterizeCsharp
             string outRasterNameGdal = @"D:\Ashis_Work\GDAL Utilities\sample-data\Utah_gdal_30m.tif";
             int rasterCellSize = 30;
 
-            SharpRasterizeLayer.VectorToRasterFromEsri(inputShapeFile, outRasterNameEsri, fieldName, rasterCellSize);
+            //SharpRasterizeLayer.VectorToRasterFromEsri(inputShapeFile, outRasterNameEsri, fieldName, rasterCellSize);
             SharpRasterizeLayer.VectorToRasterFromGdal(inputShapeFile, outRasterNameGdal, fieldName, rasterCellSize);
+
+            Console.ReadLine();
 
         }
 
