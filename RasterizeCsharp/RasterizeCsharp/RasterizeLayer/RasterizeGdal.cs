90e169c321a716984b75263545ed1ef5792a290c
unknown <aashislamsal@AFSPCHPA23810BJ.usda.net>
2015-09-03 10:44:00 -0600

    Refactored

--- RasterizeCsharp/RasterizeCsharp/RasterizeLayer/RasterizeGdal.cs
+++ RasterizeCsharp/RasterizeCsharp/RasterizeLayer/RasterizeGdal.cs
@@ -1,32 +1,14 @@
 ﻿using System;
 using System.IO;
-using ESRI.ArcGIS.ConversionTools;
-using ESRI.ArcGIS.Geoprocessor;
 using OSGeo.GDAL;
 using OSGeo.OGR;
 using OSGeo.OSR;
 
-
-namespace RasterizeCsharp
+namespace RasterizeCsharp.RasterizeLayer
 {
-    class Program
+    class RasterizeGdal
     {
-        static void Main(string[] args)
-        {
-            Program program = new Program();
-
-            string inputShapeFile = @"D:\Ashis_Work\GDAL Utilities\sample-data\UtahBoundary.shp";
-            string fieldName = "Shape_Area";
-            string outRasterNameEsri = @"D:\Ashis_Work\GDAL Utilities\sample-data\Utah_ESRI_30m.tif";
-            string outRasterNameGdal = @"D:\Ashis_Work\GDAL Utilities\sample-data\Utah_gdal_30m.tif";
-            int rasterCellSize = 30;
-
-            program.VectorToRasterFromEsri(inputShapeFile, outRasterNameEsri, fieldName, rasterCellSize);
-            program.VectorToRasterFromGdal(inputShapeFile, outRasterNameGdal, fieldName, rasterCellSize);
-
-        }
-
-        public void VectorToRasterFromGdal(string inputFeature, string outRaster, string fieldName, int cellSize)
+        public static void Rasterize(string inputFeature, string outRaster, string fieldName, int cellSize)
         {
             // Define pixel_size and NoData value of new raster
             int rasterCellSize = cellSize;
@@ -42,7 +24,7 @@ namespace RasterizeCsharp
 
             Envelope envelope = new Envelope();
             layer.GetExtent(envelope, 0);
-            
+
             //Compute the out raster cell resolutions
             int x_res = Convert.ToInt32((envelope.MaxX - envelope.MinX) / rasterCellSize);
             int y_res = Convert.ToInt32((envelope.MaxY - envelope.MinY) / rasterCellSize);
@@ -55,11 +37,11 @@ namespace RasterizeCsharp
             Gdal.AllRegister();
 
             //Check if output raster exists & delete (optional)
-            if(File.Exists(outputRasterFile))
+            if (File.Exists(outputRasterFile))
             {
                 File.Delete(outputRasterFile);
             }
-            
+
             //Create new tiff 
             OSGeo.GDAL.Driver outputDriver = Gdal.GetDriverByName("GTiff");
             Dataset outputDataset = outputDriver.Create(outputRasterFile, x_res, y_res, 1, DataType.GDT_Float64, null);
@@ -68,68 +50,42 @@ namespace RasterizeCsharp
             string inputShapeSrs;
             SpatialReference spatialRefrence = layer.GetSpatialRef();
             spatialRefrence.ExportToWkt(out inputShapeSrs);
-            
+
             //Assign input feature srs to outpur raster
             outputDataset.SetProjection(inputShapeSrs);
-            
+
             //Geotransform
             double[] argin = new double[] { envelope.MinX, rasterCellSize, 0, envelope.MaxY, 0, -rasterCellSize };
             outputDataset.SetGeoTransform(argin);
-            
+
             //Set no data
             Band band = outputDataset.GetRasterBand(1);
             band.SetNoDataValue(noDataValue);
-            
+
             //close tiff
             outputDataset.FlushCache();
             outputDataset.Dispose();
 
             //Feature to raster rasterize layer options
-            
+
             //No of bands (1)
-            int[] bandlist = new int[] { 1 }; 
-            
+            int[] bandlist = new int[] { 1 };
+
             //Values to be burn on raster (10.0)
             double[] burnValues = new double[] { 10.0 };
             Dataset myDataset = Gdal.Open(outputRasterFile, Access.GA_Update);
-            
+
             //additional options
 
             string[] rasterizeOptions;
             //rasterizeOptions = new string[] { "ALL_TOUCHED=TRUE", "ATTRIBUTE=Shape_Area" }; //To set all touched pixels into raster pixel
 
-            rasterizeOptions = new string[] { "ATTRIBUTE="+fieldName };
+            rasterizeOptions = new string[] { "ATTRIBUTE=" + fieldName };
 
             //Rasterize layer
             //Gdal.RasterizeLayer(myDataset, 1, bandlist, layer, IntPtr.Zero, IntPtr.Zero, 1, burnValues, null, null, null); // To burn the given burn values instead of feature attributes
             Gdal.RasterizeLayer(myDataset, 1, bandlist, layer, IntPtr.Zero, IntPtr.Zero, 1, burnValues, rasterizeOptions, null, null);
 
         }
-
-        public void VectorToRasterFromEsri(string inputFeature,string outRaster,string fieldName, int cellSize)
-        {
-            try
-            {
-                //Runtime manager to find the ESRI product installed in the system
-                ESRI.ArcGIS.RuntimeManager.Bind(ESRI.ArcGIS.ProductCode.Desktop);
-
-                Geoprocessor geoprocessor = new Geoprocessor();
-                geoprocessor.OverwriteOutput = true;
-                
-                FeatureToRaster featureToRaster = new FeatureToRaster();
-                featureToRaster.cell_size = cellSize;
-                featureToRaster.in_features = inputFeature;
-                featureToRaster.out_raster = outRaster;
-                featureToRaster.field = fieldName;
-
-                geoprocessor.Execute(featureToRaster, null);
-
-            }catch(Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-        }
-
     }
 }
---
+++ RasterizeCsharp/RasterizeCsharp/VectorToRaster.cs
@@ -0,0 +1,21 @@
+﻿using RasterizeCsharp.RasterizeLayer;
+
+namespace RasterizeCsharp
+{
+    class VectorToRaster
+    {
+        static void Main(string[] args)
+        {
+            string inputShapeFile = @"D:\Ashis_Work\GDAL Utilities\sample-data\UtahBoundary.shp";
+            string fieldName = "Shape_Area";
+            string outRasterNameEsri = @"D:\Ashis_Work\GDAL Utilities\sample-data\Utah_ESRI_30m.tif";
+            string outRasterNameGdal = @"D:\Ashis_Work\GDAL Utilities\sample-data\Utah_gdal_30m.tif";
+            int rasterCellSize = 30;
+
+            SharpRasterizeLayer.VectorToRasterFromEsri(inputShapeFile, outRasterNameEsri, fieldName, rasterCellSize);
+            SharpRasterizeLayer.VectorToRasterFromGdal(inputShapeFile, outRasterNameGdal, fieldName, rasterCellSize);
+
+        }
+
+    }
+}
