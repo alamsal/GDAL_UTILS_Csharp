11b1ef942d530a19c9f713d079e1cd93064ebf17
unknown <aashislamsal@AFSPCHPA23810BJ.usda.net>
2015-09-10 10:41:37 -0600

    Added standard deviation algorithm

--- RasterizeCsharp/RasterizeCsharp/ZonalIO/StatisticsExport.cs
+++ RasterizeCsharp/RasterizeCsharp/ZonalIO/StatisticsExport.cs
@@ -18,17 +18,21 @@ namespace RasterizeCsharp.ZonalIO
             Console.WriteLine("Exporting zonal statistics in csv...");
             using(var w = new StreamWriter(_csvFileName))
             {
-                string csvHeader = string.Format("{0},{1},{2},{3},{4},{5},{6}","VALUE","COUNT","MIN","MAX","MEAN","SUM","VARIETY");
+                string csvHeader = string.Format("{0},{1},{2},{3},{4},{5},{6},{7}","VALUE","COUNT","MIN","MAX","MEAN","STDEV","SUM","VARIETY");
                 w.WriteLine(csvHeader);
 
                 foreach (KeyValuePair<int, List<double>> zoneValue in zonalValues)
                 {
+                    var stDev = Math.Sqrt(zoneValue.Value.Average(z => z * z) - Math.Pow(zoneValue.Value.Average(), 2));
 
-                    string csvLine = string.Format("{0},{1},{2},{3},{4},{5},{6}", zoneValue.Key, zoneValue.Value.Count, zoneValue.Value.Min(min => Convert.ToDouble(min)), zoneValue.Value.Max(max => Convert.ToDouble(max)),
-                        zoneValue.Value.Average(avg => Convert.ToDouble(avg)), zoneValue.Value.Sum(sum => Convert.ToDouble(sum)),zoneValue.Value.Distinct().Count());
+                    string csvLine = string.Format("{0},{1},{2},{3},{4},{5},{6},{7}", zoneValue.Key, zoneValue.Value.Count, zoneValue.Value.Min(min => Convert.ToDouble(min)), zoneValue.Value.Max(max => Convert.ToDouble(max)),
+                        zoneValue.Value.Average(avg => Convert.ToDouble(avg)), stDev, zoneValue.Value.Sum(sum => Convert.ToDouble(sum)), zoneValue.Value.Distinct().Count());
 
                     w.WriteLine(csvLine);
                     w.Flush();
+
+                    
+                    
 
                     //Console.WriteLine(zoneValue.Key + " " + zoneValue.Value.Count + " " + zoneValue.Value.Sum(sum => Convert.ToDouble(sum)) + " "+ zoneValue.Value.Min(min => Convert.ToDouble(min))  );
 
