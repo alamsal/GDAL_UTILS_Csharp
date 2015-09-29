c50c337ef62fda0549daed52e02567679af21ae8
unknown <aashislamsal@AFSPCHPA23810BJ.usda.net>
2015-09-29 17:08:18 -0600

    New test parameters

--- RasterizeCsharp/RasterizeCsharp/GdalUtils.cs
+++ RasterizeCsharp/RasterizeCsharp/GdalUtils.cs
@@ -8,19 +8,22 @@ namespace RasterizeCsharp
         static void Main(string[] args)
         {
             
-            string inputShapeFile = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik\ns_lev05.shp";
-            string infieldName = "Id";
+            //string inputShapeFile = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik\ns_lev05.shp";
+            string inputShapeFile = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik2\smallish dataset\polygons.shp";
+            //string infieldName = "Id";
+            string infieldName = "OBJECTID";
             int outRasterCellSize =30;
             
-            string inValueRaster = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik\Whetstone_20080229eDOQQMos.tif";
+            
             string outZonalCsvGDAL = "gdalZonalStat.csv";
             string outZonalCsvESRI = "esriZonalStat.csv";
 
-            
+            string inValueRaster = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik2\smallish dataset\teuitest_NED_30m_30_R1C1.img";
+            //string inValueRaster = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik\Whetstone_20080229SoilEnh.img";
+            //string inValueRaster = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik\Whetstone_20080229eDOQQMos.tif";
             
             DateTime gdalStart = DateTime.Now;
-            string invalRast = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik\Whetstone_20080229SoilEnh.img";
-            ComputeStatistics.ComputeZonalStatistics(invalRast, inputShapeFile, infieldName, outRasterCellSize, outZonalCsvGDAL);
+            //ComputeStatistics.ComputeZonalStatistics(inValueRaster, inputShapeFile, infieldName, outRasterCellSize, outZonalCsvGDAL);
 
             DateTime gdalEnd = DateTime.Now;
 
@@ -29,9 +32,13 @@ namespace RasterizeCsharp
             
             DateTime esriStart = DateTime.Now;
             
-            string folder =@"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik";
-            string fileName = "Whetstone_20080229SoilEnh.img";
-            
+            //string folder =@"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik";
+            string folder = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik2\smallish dataset";
+
+            string fileName = "teuitest_NED_30m_30_R1C1.img";
+            //string fileName = "Whetstone_20080229SoilEnh.img";
+
+            //string fileName = "Whetstone_20080229eDOQQMos.tif";
 
             ZonalStatisticsEsri.OpenFileRasterDataset(folder, fileName, inputShapeFile, infieldName, outRasterCellSize, outZonalCsvESRI);
           
@@ -39,7 +46,10 @@ namespace RasterizeCsharp
 
             TimeSpan esriTimeSpan = esriEnd - esriStart;
             Console.WriteLine("Total time ESRI: {0}", esriTimeSpan);
-            
+            Console.WriteLine("\n \n");
+            Console.WriteLine(" Input shp: {0}", inputShapeFile);
+            Console.WriteLine(" Input raster: {0}", inValueRaster);
+            Console.WriteLine(" Output Cell: {0}", outRasterCellSize);
 
               
             
