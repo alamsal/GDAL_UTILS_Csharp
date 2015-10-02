dc74f469e69b9dcfd19128a5554e09f4f16966ce
unknown <aashislamsal@AFSPCHPA23810BJ.usda.net>
2015-10-02 11:37:31 -0600

    More IO files

--- RasterizeCsharp/RasterizeCsharp/GdalUtils.cs
+++ RasterizeCsharp/RasterizeCsharp/GdalUtils.cs
@@ -9,35 +9,50 @@ namespace RasterizeCsharp
         {
             
             //string inputShapeFile = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik\ns_lev05.shp";
-            string inputShapeFile = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik2\smallish dataset\polygons.shp";
+            //string inputShapeFile = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik2\smallish dataset\polygons.shp";
+            string inputShapeFile = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik2\TEUI_5__Test_data\state_boundaries_us_100k.shp";
+            //string inputShapeFile = @"D:\Ashis_Work\GDAL Utilities\sample-data\UtahGrid\Counties.shp";
+
+            //string infieldName = "FIPS";
+            string infieldName = "STATE_FIPS";
             //string infieldName = "Id";
-            string infieldName = "OBJECTID";
-            int outRasterCellSize =30;
+            //string infieldName = "OBJECTID";
+            //int outRasterCellSize =30;
+
+            int outRasterCellSize = 90;
             
             
             string outZonalCsvGDAL = "gdalZonalStat.csv";
             string outZonalCsvESRI = "esriZonalStat.csv";
 
-            string inValueRaster = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik2\smallish dataset\teuitest_NED_30m_30_R1C1.img";
+            //string inValueRaster = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik2\smallish dataset\teuitest_NED_30m_30_R1C1.img";
             //string inValueRaster = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik\Whetstone_20080229SoilEnh.img";
             //string inValueRaster = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik\Whetstone_20080229eDOQQMos.tif";
-            
+
+            string inValueRaster = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik2\TEUI_5__Test_data\slope_90m";
+            //string inValueRaster = @"D:\Ashis_Work\GDAL Utilities\sample-data\UtahGrid\dem90_utm83";
+            Console.WriteLine("GDAL working...");
             DateTime gdalStart = DateTime.Now;
             ComputeStatistics.ComputeZonalStatistics(inValueRaster, inputShapeFile, infieldName, outRasterCellSize, outZonalCsvGDAL);
-
+            
+            
             DateTime gdalEnd = DateTime.Now;
 
             TimeSpan gdalTimeSpan = gdalEnd - gdalStart;
             Console.WriteLine("Total time GDAL: {0}",gdalTimeSpan);
-            
+
+            Console.WriteLine("Esri working ...");
             DateTime esriStart = DateTime.Now;
             
             //string folder =@"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik";
-            string folder = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik2\smallish dataset";
+            //string folder = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik2\smallish dataset";
+            string folder = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik2\TEUI_5__Test_data";
+            //string folder = @"D:\Ashis_Work\GDAL Utilities\sample-data\UtahGrid";
 
-            string fileName = "teuitest_NED_30m_30_R1C1.img";
+            //string fileName = "teuitest_NED_30m_30_R1C1.img";
             //string fileName = "Whetstone_20080229SoilEnh.img";
-
+            string fileName = "slope_90m";
+            //string fileName = "dem90_utm83";
             //string fileName = "Whetstone_20080229eDOQQMos.tif";
 
             ZonalStatisticsEsri.OpenFileRasterDataset(folder, fileName, inputShapeFile, infieldName, outRasterCellSize, outZonalCsvESRI);
@@ -51,6 +66,7 @@ namespace RasterizeCsharp
             Console.WriteLine(" Input raster: {0}", inValueRaster);
             Console.WriteLine(" Output Cell: {0}", outRasterCellSize);
 
+            
               
             
             
