780117c74d5d87c4edbd2037accc5e56b8cb968b
unknown <aashislamsal@AFSPCHPA23810BJ.usda.net>
2015-10-16 16:02:16 -0600

    More dataset testing

--- RasterizeCsharp/RasterizeCsharp/GdalUtils.cs
+++ RasterizeCsharp/RasterizeCsharp/GdalUtils.cs
@@ -8,13 +8,12 @@ namespace RasterizeCsharp
         static void Main(string[] args)
         {
 
-
             //string inputShapeFile = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik\ns_lev05.shp";
             //string inputShapeFile = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik2\smallish dataset\polygons.shp";
             //string inputShapeFile = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik2\TEUI_5__Test_data\state_boundaries_us_100k.shp";
             string inputShapeFile = @"D:\Ashis_Work\GDAL Utilities\sample-data\UtahGrid\Counties.shp";
 
-            string infieldName = "COUNTYNBR";
+            string infieldName = "FIPS_STR";
             //string infieldName = "STATE_FIPS";
             //string infieldName = "Id";
             //string infieldName = "OBJECTID";
@@ -26,7 +25,7 @@ namespace RasterizeCsharp
             string outZonalCsvGDAL = "gdalZonalStat.csv";
             string outZonalCsvESRI = "esriZonalStat.csv";
 
-            /*
+            
             //string inValueRaster = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik2\smallish dataset\teuitest_NED_30m_30_R1C1.img";
             //string inValueRaster = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik\Whetstone_20080229SoilEnh.img";
             //string inValueRaster = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik\Whetstone_20080229eDOQQMos.tif";
@@ -34,22 +33,22 @@ namespace RasterizeCsharp
             //string inValueRaster = @"D:\Ashis_Work\GDAL Utilities\sample-data\FromErik2\TEUI_5__Test_data\slope_90m";
             string inValueRaster = @"D:\Ashis_Work\GDAL Utilities\sample-data\UtahGrid\dem90_utm83";
             Console.WriteLine("GDAL working...");
+            
             DateTime gdalStart1 = DateTime.Now;
-
             ComputeStatistics.ComputeZonalStatistics(inValueRaster, inputShapeFile, infieldName, outRasterCellSize, outZonalCsvGDAL);
             DateTime gdalEnd1 = DateTime.Now;
+
             string gdbPath = @"D:\Ashis_Work\GDAL Utilities\sample-data\Geodatabase\EsriZonalOutputs.gdb";
-            DateTime gdalStart2 = DateTime.Now;
-            //ComputeStatistics.ComputeZonalStatisticsUsingFeatureGdb(gdbPath, "Counties", inValueRaster, infieldName, outRasterCellSize, outZonalCsvGDAL);
-            DateTime gdalEnd2 = DateTime.Now;
-
             
-
+            DateTime gdalStart2 = DateTime.Now;
+            ComputeStatistics.ComputeZonalStatisticsUsingFeatureGdb(gdbPath, "Counties", inValueRaster, infieldName, outRasterCellSize, outZonalCsvGDAL);
+            DateTime gdalEnd2 = DateTime.Now;
+            
             TimeSpan gdalTimeSpan1 = gdalEnd1 - gdalStart1;
             TimeSpan gdalTimeSpan2 = gdalEnd2 - gdalStart2;
-
             Console.WriteLine("Total time GDAL: outside gdb- {0} inside gdb- {1}", gdalTimeSpan1,gdalTimeSpan2);
-            */
+            
+
             Console.WriteLine("Esri working ...");
             DateTime esriStart = DateTime.Now;
             
@@ -72,16 +71,10 @@ namespace RasterizeCsharp
             Console.WriteLine("Total time ESRI: {0}", esriTimeSpan);
             Console.WriteLine("\n \n");
             Console.WriteLine(" Input shp: {0}", inputShapeFile);
-            
             Console.WriteLine(" Output Cell: {0}", outRasterCellSize);
 
             
-            
-
-
-            Environment.Exit(0);
-            //Console.WriteLine("Done");
-           
+            Console.WriteLine("Done");
 
 
         }
